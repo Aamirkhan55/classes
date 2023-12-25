31464a058986d21c948926282a073f5c156ebb0e
Amir55143 <aamirnoor55143@gmail.com>
2023-12-25 17:10:57 +0500

    Mathimatices

--- bin/classes.dart
+++ bin/classes.dart
@@ -1,5 +1,35 @@
-import 'package:classes/classes.dart' as classes;
+void main() {
+  Mathematics mathematics = Mathematics();
+  int addResult  = (mathematics.addition(4, 6));
+  print(addResult);
 
-void main(List<String> arguments) {
-  print('Hello world: ${classes.calculate()}!');
+  int subResult  = (mathematics.subtraction(4, 6));
+  print(subResult);
+
+  int mulResult  = (mathematics.multiplication(4, 6));
+  print(mulResult);
+
+  double diviResult  = (mathematics.division(4, 6));
+  print(diviResult);
+
+}
+
+class Mathematics {
+  // Addition
+  int addition (int a1, int a2) {
+    return a1 +  a2;
+  }
+  // Subtraction
+  int subtraction (int a1, int a2) {
+    return a1 -  a2;
+  }
+  // Mutiplication
+  int multiplication (int a1, int a2) {
+    return a1 *  a2;
+  }
+  // Division 
+  double division (double a1, int a2) {
+    return a1 /  a2;
+  }
+
 }
